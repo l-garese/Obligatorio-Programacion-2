5d5800e996d562e8bdd220a424e77c2562f11a7c
Milena Blanco <mblanco7@correo.um.edu.uy>
2026-06-19 15:27:23 -0300

    Test de finish y terminate, stack overflow y no running processes

--- obligatorio2026/src/main/java/uy/edu/um/doors/ProcessManager.java
+++ obligatorio2026/src/main/java/uy/edu/um/doors/ProcessManager.java
@@ -1,5 +1,7 @@
 package uy.edu.um.doors;
 
+import exceptions.NoRunningProcessException;
+import exceptions.UsuarioNoEncontradoException;
 import uy.edu.um.tad.queue.EmptyQueueException;
 import uy.edu.um.tad.stack.EmptyStackException;
 
@@ -8,9 +10,9 @@ public interface ProcessManager {
     public void loadProcessAndUserData(String processCsvPath, String usersCsvPath);
     public void prepareProcesses() throws Exception;
     public void executeNextProcess();
-    public void finishProcessOk() throws EmptyStackException;
-    public void finishProcessError() throws EmptyStackException;
-    public void terminateProcess(int uid) throws EmptyStackException;
+    public void finishProcessOk() throws EmptyStackException, NoRunningProcessException;
+    public void finishProcessError() throws EmptyStackException, NoRunningProcessException;
+    public void terminateProcess(int uid) throws EmptyStackException, NoRunningProcessException, UsuarioNoEncontradoException;
     public void printStatus();
     public void printStatusVerbose();
     public void printStatusByUser(int uid);
--- obligatorio2026/src/test/java/uy/edu/um/doors/ProcessManagerImplTest.java
+++ obligatorio2026/src/test/java/uy/edu/um/doors/ProcessManagerImplTest.java
@@ -1,5 +1,7 @@
 package uy.edu.um.doors;
 
+import exceptions.NoRunningProcessException;
+import exceptions.UsuarioNoEncontradoException;
 import org.junit.jupiter.api.Test;
 import uy.edu.um.tad.heap.EmptyHeapException;
 import uy.edu.um.tad.heap.MyHeap;
@@ -8,6 +10,7 @@ import uy.edu.um.tad.list.MyLinkedListImpl;
 import uy.edu.um.tad.list.MyList;
 import uy.edu.um.tad.queue.MyQueue;
 import uy.edu.um.tad.queue.MyQueueImpl;
+import uy.edu.um.tad.stack.MyStack;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -59,15 +62,117 @@ class ProcessManagerImplTest {
 
 
     @Test
-    void finishProcessOk() {
+    void finishProcessOk() throws Exception {
+        ProcessManagerImpl manager=new ProcessManagerImpl();
+        MyHeap<DoorProcess> procesospendientes=manager.getPendingProcesses();
+        MyQueue<DoorProcess> procesosNuevos= manager.getNew_processes();
+        MyStack<DoorProcess> procesosFinalizados = manager.getFinished_processes();
+        MyList<Event> eventos1=new MyLinkedListImpl<>();
+        User usuario1=new User(111,"Usuario1", User.UserType.ADMIN);
+        DoorProcess proceso1=new DoorProcess(1,"Proceso1",usuario1,eventos1);
+        procesosNuevos.enqueue(proceso1);
+        manager.prepareProcesses();
+        manager.executeNextProcess();
+        manager.finishProcessOk();
+        DoorProcess procesoFinalizado =procesosFinalizados.peek();
+        assertEquals(DoorProcess.FinishedState.OK, procesoFinalizado.getfinishedState());
+        assertEquals(DoorProcess.ProcessState.FINISHED, procesoFinalizado.getProcessState());
     }
 
     @Test
-    void finishProcessError() {
+    void finishProcessError() throws Exception {
+        ProcessManagerImpl manager=new ProcessManagerImpl();
+        MyQueue<DoorProcess> procesosNuevos= manager.getNew_processes();
+        MyStack<DoorProcess> procesosFinalizados = manager.getFinished_processes();
+        MyList<Event> eventos1=new MyLinkedListImpl<>();
+        User usuario1=new User(111,"Usuario1", User.UserType.ADMIN);
+        DoorProcess proceso1=new DoorProcess(1,"Proceso1",usuario1,eventos1);
+        procesosNuevos.enqueue(proceso1);
+        manager.prepareProcesses();
+        manager.executeNextProcess();
+        manager.finishProcessError();
+        DoorProcess procesoFinalizado =procesosFinalizados.peek();
+        assertEquals(DoorProcess.FinishedState.ERROR, procesoFinalizado.getfinishedState());
+        assertEquals(DoorProcess.ProcessState.FINISHED, procesoFinalizado.getProcessState());
     }
 
     @Test
-    void terminateProcess() {
+    void terminateProcess() throws Exception {
+        ProcessManagerImpl manager = new ProcessManagerImpl();
+        MyQueue<DoorProcess> procesosNuevos = manager.getNew_processes();
+        MyStack<DoorProcess> procesosFinalizados = manager.getFinished_processes();
+        MyList<Event> eventos1 = new MyLinkedListImpl<>();
+        User usuario1 = new User(111, "Usuario1", User.UserType.ADMIN);
+        // Registrar el usuario en el hash del manager
+        manager.getUserByUID().put(111, usuario1);
+        DoorProcess proceso1 = new DoorProcess(1, "Proceso1", usuario1, eventos1);
+        procesosNuevos.enqueue(proceso1);
+        manager.prepareProcesses();
+        manager.executeNextProcess();
+        manager.terminateProcess(111);
+        DoorProcess procesoFinalizado = procesosFinalizados.peek();
+        assertEquals(DoorProcess.FinishedState.TERMINATED, procesoFinalizado.getfinishedState());
+        assertEquals(DoorProcess.ProcessState.FINISHED, procesoFinalizado.getProcessState());
+        assertEquals(usuario1, procesoFinalizado.getTerminadoPor());
+    }
+
+    @Test
+    void terminateProcessUsuarioNulo() throws Exception{
+        ProcessManagerImpl manager=new ProcessManagerImpl();
+        MyQueue<DoorProcess> procesosNuevos= manager.getNew_processes();
+        MyList<Event> eventos1=new MyLinkedListImpl<>();
+        User usuario1 = new User(111, "Usuario1", User.UserType.ADMIN);
+        DoorProcess proceso1=new DoorProcess(1,"Proceso1",usuario1,eventos1);
+        procesosNuevos.enqueue(proceso1);
+        manager.prepareProcesses();
+        manager.executeNextProcess();
+        assertThrows(UsuarioNoEncontradoException.class, () -> manager.terminateProcess(999));
+    }
+
+    @Test
+    void StackLleno() throws Exception {
+        ProcessManagerImpl manager = new ProcessManagerImpl();
+        MyQueue<DoorProcess> procesosNuevos = manager.getNew_processes();
+        MyStack<DoorProcess> procesosFinalizados = manager.getFinished_processes();
+        MyList<Event> eventos1 = new MyLinkedListImpl<>();
+        User usuario1 = new User(111, "Usuario1", User.UserType.ADMIN);
+
+        DoorProcess proceso1 = new DoorProcess(1, "Proceso1", usuario1, eventos1);
+        DoorProcess proceso2 = new DoorProcess(2, "Proceso2", usuario1, eventos1);
+        DoorProcess proceso3 = new DoorProcess(3, "Proceso3", usuario1, eventos1);
+        DoorProcess proceso4 = new DoorProcess(4, "Proceso4", usuario1, eventos1);
+        procesosNuevos.enqueue(proceso1);
+        procesosNuevos.enqueue(proceso2);
+        procesosNuevos.enqueue(proceso3);
+        manager.prepareProcesses();
+        manager.executeNextProcess();
+        manager.finishProcessOk();
+        manager.executeNextProcess();
+        manager.finishProcessOk();
+        manager.executeNextProcess();
+        manager.finishProcessOk();
+        // El stack ahora está lleno (3 procesos)
+        assertEquals(3, procesosFinalizados.size());
+        procesosNuevos.enqueue(proceso4); //overflow
+        manager.prepareProcesses();
+        manager.executeNextProcess();
+        manager.finishProcessOk();
+        // Después del overflow el stack se vació y tiene solo el nuevo proceso
+        assertEquals(1, procesosFinalizados.size());
+        assertEquals(proceso4, procesosFinalizados.peek());
+    }
+
+    @Test
+    void finishProcessErrorSinRunning() {
+        ProcessManagerImpl manager = new ProcessManagerImpl();
+        assertThrows(NoRunningProcessException.class, () -> manager.finishProcessError());
+    }
+
+    @Test
+    void terminateProcessSinRunning() {
+        ProcessManagerImpl manager = new ProcessManagerImpl();
+        manager.getUserByUID().put(111, new User(111, "Usuario1", User.UserType.ADMIN));
+        assertThrows(NoRunningProcessException.class, () -> manager.terminateProcess(111));
     }
 
     @Test
